e5d78010c7e3e5ab7030e10f7516b3ed8f5d64c7
Peter Newman <peternewman@users.noreply.github.com>
2018-08-19 15:11:52 +0100

    Add the Travis shell script

---
+++ .travis-ci.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# This script is triggered from the script section of .travis.yml
+# It runs the appropriate commands depending on the task requested.
+
+set -e
+
+SPELLINGBLACKLIST=$(cat <<-BLACKLIST
+      -wholename "./.codespellignore" -or \
+      -wholename "./.git/*"
+BLACKLIST
+)
+
+if [[ $TASK = 'codespell' ]]; then
+  # run codespell only if it is the requested task
+  # count the number of codespell errors
+  spellingerrors=$(zrun codespell --check-filenames --check-hidden --quiet 2 --regex "[a-zA-Z0-9][\\-'a-zA-Z0-9]+[a-zA-Z0-9]" --exclude-file .codespellignore $spellingfiles 2>&1 | wc -l)
+  if [[ $spellingerrors -ne 0 ]]; then
+    # print the output for info
+    zrun codespell --check-filenames --check-hidden --quiet 2 --regex "[a-zA-Z0-9][\\-'a-zA-Z0-9]+[a-zA-Z0-9]" --exclude-file .codespellignore $spellingfiles
+    echo "Found $spellingerrors spelling errors via codespell"
+    exit 1;
+  else
+    echo "Found $spellingerrors spelling errors via codespell"
+  fi;
+else
+  # Otherwise compile and check as normal
+  platformio ci --lib="." --board=$BOARD
+fi
