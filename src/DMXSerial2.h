d7e921420b68ce4a8da72090ee377650911e26fc
TimMJN <75533580+TimMJN@users.noreply.github.com>
2023-06-06 19:37:53 +0200

    Update src/DMXSerial2.h
    Co-authored-by: Peter Newman <peternewman@users.noreply.github.com>

--- src/DMXSerial2.h
+++ src/DMXSerial2.h
@@ -223,8 +223,8 @@ class DMXSerialClass2
      * @param [in] func Callback function for answering on device specific features.
      * @param [in] sensorFunc Callback function for retrieving a sensor value.
      * @param [in] modePin The pin used to switch the communication direction. This parameter is optiona and defaults to 2.
-     * @param [in] modeIn  The level for inbound communication. This parameter is optiona and defaults to 0 = LOW.
-     * @param [in] modeOut The level for outbound communication. This parameter is optiona and defaults to 1 = HIGH.
+     * @param [in] modeIn  The level for inbound communication. This parameter is optional and defaults to 0 = LOW.
+     * @param [in] modeOut The level for outbound communication. This parameter is optional and defaults to 1 = HIGH.
      */
     void    init (struct RDMINIT *initData, RDMCallbackFunction func, RDMGetSensorValue sensorFunc, uint8_t modePin = 2, uint8_t modeIn = 0, uint8_t modeOut = 1) {
       init(initData, func, sensorFunc, NULL, NULL, modePin, modeIn, modeOut);
